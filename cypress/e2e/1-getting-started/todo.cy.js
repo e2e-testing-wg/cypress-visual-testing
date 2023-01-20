42c1d1e34136521565c328e9b9e1c5919a401f0d
Joan León <joan.leon@gmail.com>
2023-01-20 22:59:51 +0100

    Change new item test

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -88,7 +88,7 @@ describe("example to-do app", () => {
   });
 
   it("can add new todo items", () => {
-    const newItem = "Feed the unicorns";
+    const newItem = "Feed the lions";
     cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
     cy.get(".todo-list li")
       .should("have.length", 3)
